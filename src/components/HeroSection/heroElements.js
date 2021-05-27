253bf1f0d20c86071d25764f799604f05b68d088
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-05-27 17:51:58 +0500

    HeroSection Content

--- src/components/HeroSection/heroElements.js
+++ src/components/HeroSection/heroElements.js
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import { Link as ScrollLink } from "react-scroll"
+import { MdArrowForward, MdKeyboardArrowRight } from "react-icons/md"
 
 export const HeroContainer = styled.div`
   background: #0c0c0c;
@@ -6,9 +8,25 @@ export const HeroContainer = styled.div`
   justify-content: center;
   align-items: center;
   padding: 0 30px;
-  height: 800px;
+  height: calc(100vh / 1.4);
   position: relative;
   z-index: 1;
+
+  ::before {
+    /* content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: linear-gradient(
+        180deg,
+        rgba(0, 0, 0, 0.2) 0%,
+        rgba(0, 0, 0, 0.6) 100%
+      ),
+      linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
+    z-index: 2; */
+  }
 `
 export const HeroBg = styled.div`
   position: absolute;
@@ -25,4 +43,93 @@ export const VideoBg = styled.video`
   --o-object-fit: cover;
   object-fit: cover;
   background: #232a34;
+`
+
+export const Filter = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  overflow: hidden;
+  z-index: 1;
+  background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.2) 0%,
+      rgba(0, 0, 0, 0.6) 100%
+    ),
+    linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
+`
+export const HeroContent = styled.div`
+  position: absolute;
+  padding: 8px 24px;
+  max-width: 1200px;
+  z-index: 3;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+export const HeroHeading = styled.h1`
+  color: #fff;
+  font-size: 48px;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    font-size: 40px;
+  }
+  @media screen and (max-width: 480) {
+    font-size: 32px;
+  }
+`
+
+export const HeroParagraph = styled.p`
+  margin-top: 24px;
+  color: #fff;
+  font-size: 24px;
+  text-align: center;
+  max-width: 600px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 24px;
+  }
+  @media screen and (max-width: 480) {
+    font-size: 18px;
+  }
+`
+
+export const Buttonwrapper = styled.div`
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+export const Button = styled(ScrollLink)`
+  border-radius: 50px;
+  background: ${({ primary }) => (primary ? "#01bf71" : "#010606")};
+  white-space: nowrap;
+  padding: ${({ big }) => (big ? "14px 48px" : "12px 30px")};
+  color: ${({ dark }) => (dark ? "#010606" : "#fff")};
+  font-size: ${({ fontBig }) => (fontBig ? "20px" : "16px")};
+  outline: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: ${({ primary }) => (primary ? "#fff" : "#01bf71")};
+    color: ${({ primary }) => (primary ? "#010606" : "#fff")};
+  }
+`
+
+export const ArrowForward = styled(MdArrowForward)`
+  margin-left: 8px;
+  font-size: 20px;
+`
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+  margin-left: 8px;
+  font-size: 20px;
 `
--- src/components/HeroSection/heroSection.js
+++ src/components/HeroSection/heroSection.js
@@ -1,10 +1,25 @@
-import React from "react"
-import { HeroBg, HeroContainer, VideoBg } from "./heroElements"
+import React, { useState } from "react"
+import {
+  Filter,
+  HeroBg,
+  HeroContainer,
+  VideoBg,
+  HeroContent,
+  HeroHeading,
+  HeroParagraph,
+  Buttonwrapper,
+  Button,
+  ArrowForward,
+  ArrowRight,
+} from "./heroElements"
 import { graphql, useStaticQuery } from "gatsby"
 
 // import Video from "../../videos/video.mp4"
 
 const HeroSection = props => {
+  // STATE
+  const [hover, setHover] = useState(false)
+
   const data = useStaticQuery(graphql`
     {
       contentfulHome(section: { eq: "heroSection" }) {
@@ -17,11 +32,37 @@ const HeroSection = props => {
     }
   `)
   const url = data.contentfulHome.video.file.url
+
+  // HANDLER FUNCTIONS
+
+  const hoverHandler = () => {
+    setHover(!hover)
+  }
+
   return (
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={url} type="video/mp4" />
+        <Filter />
       </HeroBg>
+      <HeroContent>
+        <HeroHeading>Virtual World</HeroHeading>
+        <HeroParagraph>
+          Sign up for a new account today and receive $250 in credit toward your
+          next payment
+        </HeroParagraph>
+        <Buttonwrapper>
+          <Button
+            to="signup"
+            // primary
+            // dark
+            onMouseEnter={hoverHandler}
+            onMouseLeave={hoverHandler}
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </Buttonwrapper>
+      </HeroContent>
     </HeroContainer>
   )
 }
