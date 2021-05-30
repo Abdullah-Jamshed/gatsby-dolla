ecf4eebb3c9785db57e9cc7f64df826146f36f80
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-05-30 17:20:03 +0500

    footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,8 +1,94 @@
 import React from "react"
-import { FooterElements } from "./footerElements"
+import { FaFacebook, FaYoutube } from "react-icons/fa"
+import {
+  FooterContainer,
+  FooterLink,
+  FooterLinkItems,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkTitle,
+  FooterWrap,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+} from "./footerElements"
 
 const Footer = () => {
-  return <FooterElements>© {new Date().getFullYear()}, DOLLA</FooterElements>
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signIn">About us</FooterLink>
+              <FooterLink to="/signIn">How its Work</FooterLink>
+              <FooterLink to="/signIn">Testimonials</FooterLink>
+              <FooterLink to="/signIn">Careers</FooterLink>
+              <FooterLink to="/signIn">Investors</FooterLink>
+              <FooterLink to="/signIn">Terms of Services</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signIn">About us</FooterLink>
+              <FooterLink to="/signIn">How its Work</FooterLink>
+              <FooterLink to="/signIn">Testimonials</FooterLink>
+              <FooterLink to="/signIn">Careers</FooterLink>
+              <FooterLink to="/signIn">Investors</FooterLink>
+              <FooterLink to="/signIn">Terms of Services</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signIn">About us</FooterLink>
+              <FooterLink to="/signIn">How its Work</FooterLink>
+              <FooterLink to="/signIn">Testimonials</FooterLink>
+              <FooterLink to="/signIn">Careers</FooterLink>
+              <FooterLink to="/signIn">Investors</FooterLink>
+              <FooterLink to="/signIn">Terms of Services</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/signIn">About us</FooterLink>
+              <FooterLink to="/signIn">How its Work</FooterLink>
+              <FooterLink to="/signIn">Testimonials</FooterLink>
+              <FooterLink to="/signIn">Careers</FooterLink>
+              <FooterLink to="/signIn">Investors</FooterLink>
+              <FooterLink to="/signIn">Terms of Services</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">DOLLA</SocialLogo>
+            <WebsiteRights>
+              DOLLA © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink
+                href="https://www.facebook.com/"
+                target="_blank"
+                aria_label="Facebook"
+              >
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink
+                href="https://www.youtube.com/"
+                target="_blank"
+                aria_label="Youtube"
+              >
+                <FaYoutube />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  )
 }
 
 export default Footer
---
+++ src/pages/signIn.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/Layout/layout"
+
+const Signin = () => {
+  return (
+    <Layout>
+      <div style={{ height: "100vh", zIndex: 1, background: "red" }}>
+        <div></div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Signin
